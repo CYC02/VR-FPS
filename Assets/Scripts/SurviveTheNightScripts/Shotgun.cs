455d46dd6f442e66d17aa88aadae157d0488348e
Cindy Chan <cindy.y.chan80@gmail.com>
2024-03-30 13:12:18 -0400

    Modify changes to shotgun so it worked with updated mesh

--- Assets/Scripts/SurviveTheNightScripts/Shotgun.cs
+++ Assets/Scripts/SurviveTheNightScripts/Shotgun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.XR;
@@ -8,21 +9,8 @@ using UnityEngine.XR;
 //reference: https://www.youtube.com/watch?v=1gPLfY93JHk
 public class Shotgun : MonoBehaviour
 {
-    /*
-    private Transform gunPoint;
-    
-    private Vector3 leftRayDirection;
-    private Vector3 rightRayDirection;
-    private Vector3 topRayDirection;
-    private Vector3 bottomRayDirection;
-    
-    private float rayLengthForward = 10f;
-    private float rayLengthOuter = 7.5f;
-    private int forwardDamage = 10;
-    private int outerDamage = 6;
-    */
-
     [SerializeField] private Transform gunPoint;
+    private bool IsActivated = false;
 
     // Start is called before the first frame update
 
@@ -35,29 +23,31 @@ public class Shotgun : MonoBehaviour
     void Update()
     {
         List<InputDevice> m_device = new List<InputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right, m_device);
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Right, m_device);
         
         if (m_device.Count == 1) {
             InputDevice device = m_device[0];
             //One right side device found
             bool triggerButtonDown = false;
             bool gripButtonDown = false;
-            if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerButtonDown) && triggerButtonDown) {
-                Debug.Log("Right trigger button pressed");
 
                 //if gripping the gun and pressed the trigger button, then shoot
                 if (device.TryGetFeatureValue(CommonUsages.gripButton, out gripButtonDown) && gripButtonDown) {
-                    Shoot();
+                Debug.Log("Grip button pressed");
+                    if (device.TryGetFeatureValue(CommonUsages.triggerButton, out triggerButtonDown) && triggerButtonDown) {
+                        Debug.Log("Right trigger button pressed");
+                        if (IsActivated) {
+                            Shoot();
+                        }
+                    }
                 }
                 
-            }
-            
         }
 
         //Shoot();
     }
 
-    public void Shoot()
+    private void Shoot()
     {
         
         Debug.Log("Shoot shotgun!");
@@ -68,7 +58,7 @@ public class Shotgun : MonoBehaviour
         Vector3 topRayDirection = gunPoint.forward + new Vector3(0f, 0.2f, 0f);
         Vector3 bottomRayDirection = gunPoint.forward + new Vector3(0f, -0.2f, 0f);
         float rayLengthForward = 10f;
-        float rayLengthOuter = 7.5f;
+        float rayLengthOuter = 7.5f; //left,right,top, and bottom ray length
         int forwardDamage = 10;
         int outerDamage = 6;
         
@@ -114,6 +104,11 @@ public class Shotgun : MonoBehaviour
             }
 
         }
-        Debug.DrawLine(rayStart, rayEndPoint, c);
+        //Debug.DrawLine(rayStart, rayEndPoint, c);
+    }
+
+    public void OnIsActivated(bool activate) {
+        IsActivated = activate;
+        Debug.Log("Activate shotgun: " + activate);
     }
 }
