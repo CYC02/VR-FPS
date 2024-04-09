5718960d2e2ee6a45f22f081bab7559edc3f71f1
Cindy Chan <cindy.y.chan80@gmail.com>
2024-04-08 22:52:19 -0400

    Modify slug box

--- Assets/Scripts/SurviveTheNightScripts/Shotgun/SlugBox.cs
+++ Assets/Scripts/SurviveTheNightScripts/Shotgun/SlugBox.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
+/*
+ * Author: Cindy Chan
+ * This script is used for making the box open and give the player ammo.
+ */
 public class SlugBox : MonoBehaviour
 {
     [SerializeField] private GameObject closedBox;
     [SerializeField] private GameObject openedBox;
+    [SerializeField] private XRGrabInteractable interactable;
+    [SerializeField] private GameObject slugAmmoPrefab;
     private bool isOpened = false;
+
+    private void Start()
+    {
+        //interactable.hoverExited.AddListener(OnHoverExit);
+    }
+
     //Switch from closed box to opened box
     public void OpenBox() { 
         closedBox.SetActive(false);
@@ -14,18 +26,29 @@ public class SlugBox : MonoBehaviour
         isOpened = true;
     }
 
-    public void CloseBox()
-    {
-        closedBox.SetActive(true);
-        openedBox.SetActive(false);
-        isOpened = false;
-    }
-
-    public void SetActivationState(bool activate) { 
+    //Activate event triggers this function to open the box
+    public void OnActivate(bool activate) {
         if (activate)
         {
             OpenBox();
         }
     }
-    
+
+    //Hover exit event triggers this function put a slug in the hand
+    public void OnHoverExit(HoverExitEventArgs args) {
+        IXRHoverInteractor interactor = args.interactorObject;
+        Debug.Log("Hovering: " + interactor);
+        XRDirectInteractor directInteractor= interactor as XRDirectInteractor;
+        if (isOpened) {
+            //hand has ammo
+            /*
+            if (directInteractor != null) {
+                GameObject ammoInstance = Instantiate(slugAmmoPrefab);
+                if (ammoInstance) {
+                    directInteractor.firstInteractableSelected = ammoInstance;
+                }
+            }
+            */
+        }
+    }
 }
