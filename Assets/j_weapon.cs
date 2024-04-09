f8d868ab577bd940240d5015485c8ce2ff6b0c19
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-04-08 22:57:47 -0400

    cleaning up some objects, working on weapon

--- Assets/j_weapon.cs
+++ Assets/j_weapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnAmmoThroughWindow : MonoBehaviour
+public class j_weapon : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
