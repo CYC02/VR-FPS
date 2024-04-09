ff7147a9d3b58fae4d5dfc8fbdb21ca42a0cfca5
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-04-08 22:42:16 -0400

    creating an ammo spawner

---
+++ Assets/j_spawn_ammo.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class j_spawn_ammo : MonoBehaviour
+{
+    public GameObject ammoBox;
+
+    private bool spawnedAmmo = false;
+    private float ammoSpawnRate = 10f;
+    private float nextSpawnAttempt = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnAmmoRandomly();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spawnedAmmo)
+        {
+            if (Time.time > nextSpawnAttempt)
+            {
+                nextSpawnAttempt = Time.time + ammoSpawnRate;
+                SpawnAmmoRandomly();
+                nextSpawnAttempt = 0f;
+            }
+        }
+    }
+
+    void SpawnAmmoRandomly()
+    {
+        int randomizeNumber = Random.Range(0, 20);
+        int chosenRandomNumber = Random.Range(0, 20);
+        Debug.Log(randomizeNumber);
+        Debug.Log(chosenRandomNumber);
+
+        if (randomizeNumber == chosenRandomNumber)
+        {
+            var position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            Instantiate(ammoBox, position, Quaternion.identity);
+            spawnedAmmo = true;
+            Debug.Log("Spawned");
+        }
+        else
+        {
+            spawnedAmmo = false;
+        }
+    }
+}
