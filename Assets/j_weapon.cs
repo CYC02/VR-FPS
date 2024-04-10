1fcc19a98ec83a33dd2318b50ba127c27a47a0fe
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2024-04-09 23:02:09 -0400

    Gun is reloadable

--- Assets/j_weapon.cs
+++ Assets/j_weapon.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
 public class j_weapon : MonoBehaviour
 {
     public GameObject bullet;
     public Transform bullet_spawn_area;
 
+    public XRSocketInteractor ammo_socket;
+
     float bullet_force = 8f;
+
+    int ammo_count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ammo_socket.onSelectEntered.AddListener(LoadWeapon);
     }
 
     // Update is called once per frame
@@ -21,11 +27,24 @@ public class j_weapon : MonoBehaviour
         
     }
 
+    public void LoadWeapon(XRBaseInteractable ammo)
+    {
+        ammo_count += 1;
+        
+        Destroy(ammo.gameObject);
+        Debug.Log(ammo_count);
+    }
+
     public void ShootWeapon()
     {
-        var position = new Vector3(bullet_spawn_area.position.x, bullet_spawn_area.position.y, bullet_spawn_area.position.z);
-        var b = Instantiate(bullet, position, Quaternion.identity);
-        var b_rigidbody = b.GetComponent<Rigidbody>();
-        b_rigidbody.AddForce(bullet_spawn_area.forward * bullet_force, ForceMode.Impulse);
+        if (ammo_count > 0)
+        {
+            var position = new Vector3(bullet_spawn_area.position.x, bullet_spawn_area.position.y, bullet_spawn_area.position.z);
+            var b = Instantiate(bullet, position, Quaternion.identity);
+            var b_rigidbody = b.GetComponent<Rigidbody>();
+            b_rigidbody.AddForce(bullet_spawn_area.forward * bullet_force, ForceMode.Impulse);
+            ammo_count -= 1;
+        }
+
     }  
 }
