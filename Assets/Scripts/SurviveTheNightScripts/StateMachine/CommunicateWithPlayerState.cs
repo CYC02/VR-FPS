ae484b5deae06468be6f8a54d00d03679e79af66
Cindy Chan <cindy.y.chan80@gmail.com>
2024-04-30 15:24:45 -0400

    Debug bobby's communication state

--- Assets/Scripts/SurviveTheNightScripts/StateMachine/CommunicateWithPlayerState.cs
+++ Assets/Scripts/SurviveTheNightScripts/StateMachine/CommunicateWithPlayerState.cs
@@ -11,16 +11,24 @@ using UnityEngine;
 public class CommunicateWithPlayerState : State
 {
     public IdleState idleState;
-    
+
+    /*
     public float noLookWaitTime = 10f;
     private float lastNoLookWaitTime;
 
     public float holdWaitTime = 3f;
     private float lastHoldTime;
+    */
+
+    public float hoverDuration = 5f;
+    public float notHoverDuration = 3f;
+    private float hoverTimer = 0f;
+    private float notHoverTimer = 0f;
     public override State RunCurrentState()
     {
         SetAnimationTrigger("Idle");
         //player gaze is true and switched to this state
+        Debug.Log(hoverTimer);
         if (bobbyGaze != null) {
             if (bobbyGaze.isHovered)
             {
@@ -29,7 +37,7 @@ public class CommunicateWithPlayerState : State
 
                 //if the player is holding an object that Bobby can take, then if the player holds it for 5 sec
                 //bobby extends his arm to player
-
+                /*
                 if (Time.time > holdWaitTime + holdWaitTime) {
                     lastHoldTime= Time.time;
 
@@ -38,7 +46,14 @@ public class CommunicateWithPlayerState : State
                     SetAnimationTrigger("ExtendLeftHand");
 
                 }
-                return this;
+                */
+                notHoverTimer= 0f;
+                hoverTimer += Time.deltaTime;
+                if (hoverTimer >= hoverDuration) {
+                    ResetAnimationTrigger("Idle");
+                    SetAnimationTrigger("ExtendLeftHand");
+                }
+
             }
             else {
                 //after a certain amount of time, the player doesn't look at Bobby,
@@ -52,9 +67,14 @@ public class CommunicateWithPlayerState : State
                     return idleState;
                 }
                 */
-                ResetAnimationTrigger("ExtendLeftHand");
-                SetAnimationTrigger("RetractLeftHand");
+                hoverTimer = 0f;
+                notHoverTimer += Time.deltaTime;
+                if (notHoverTimer >= notHoverDuration) {
+                    ResetAnimationTrigger("ExtendLeftHand");
+                    SetAnimationTrigger("RetractLeftHand");
+                }
 
+                
             }
         }
         
