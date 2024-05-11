38ddcc9eae0edea6933ea4701d2eda0855667902
ZhangBenjamin <bluebenjamin123@gmail.com>
2024-05-11 16:51:46 -0400

    Added animation for shark and swimming code

--- Assets/Scripts/DivingAnimation.cs
+++ Assets/Scripts/DivingAnimation.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class DivingAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Values")]
+    [SerializeField] float swimForce = 2f;
+    [SerializeField] float dragForce = 1f;
+    [SerializeField] float minForce;
+    [SerializeField] float minTimeBetweenStrokes;
+    [Header("References")]
+    [SerializeField] InputActionReference leftControllerSwimReference;
+    [SerializeField] InputActionReference leftControllerVelocity;
+    [SerializeField] InputActionReference rightControllerSwimReference;
+    [SerializeField] InputActionReference rightControllerVelocity;
+    [SerializeField] Transform trackingReference;
+
+    Rigidbody _rigidbody;
+    float _cooldownTimer;
+
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _cooldownTimer += Time.fixedDeltaTime;
+        if (_cooldownTimer > minTimeBetweenStrokes
+            && leftControllerSwimReference.action.IsPressed()
+            && rightControllerSwimReference.action.IsPressed())
+        {
+            var leftHandVelocity = leftControllerVelocity.action.ReadValue<Vector3>();
+            var rightHandVelocity = rightControllerVelocity.action.ReadValue<Vector3>();
+            Vector3 localVelocity = leftHandVelocity + rightHandVelocity;
+            localVelocity *= -1;
+
+            if(localVelocity.sqrMagnitude > minForce * minForce)
+            {
+                Vector3 worldVelocity = trackingReference.TransformDirection(localVelocity);
+                _rigidbody.AddForce(worldVelocity * swimForce, ForceMode.Acceleration);
+                _cooldownTimer = 0f;
+            }
+
+        }
+
+        if (_rigidbody.velocity.sqrMagnitude > 0.01f)
+        {
+            _rigidbody.AddForce(-_rigidbody.velocity * dragForce, ForceMode.Acceleration);
+        }
     }
 }
+
+
